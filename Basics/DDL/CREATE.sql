b73bbb84a714146e6eef50415b7318f55fe48b8c
Muhammad <mukhammad.developer@outlook.com>
2024-05-20 06:53:42 -0700

    creating with constrians

--- Basics/DDL/CREATE.sql
+++ Basics/DDL/CREATE.sql
@@ -9,5 +9,18 @@ CREATE TABLE STUDENT (
     Lastname  VARCHAR(100) 
     Email VARCHAR (100),
     Username VARCHAR(100)
-    
+
+)
+
+-- Creating with constains --
+
+CREATE TABLE Student (
+
+    Id INT PRIMARY KEY,
+    Firstname VARCHAR(100) NOT NULL,
+    Lastname VARCHAR(100) NOT NULL,
+    Age int CHECK(Age >= 18 AND Age <= 100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Country VARCHAR(100) NOT NULL DEFAULT 'UZB',
+
 )
