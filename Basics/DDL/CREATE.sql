3fa2e78114924a0935b163b8d467b9e3184be70d
Muhammad <mukhammad.developer@outlook.com>
2024-05-20 06:39:28 -0700

    create

---
+++ Basics/DDL/CREATE.sql
@@ -0,0 +1,13 @@
+-- Query how to create new table in database --
+
+CREATE TABLE STUDENT (
+
+--  name  | data - type 
+
+    Id  INT PRIMARY KEY,
+    Firstname VARCHAR(100),
+    Lastname  VARCHAR(100) 
+    Email VARCHAR (100),
+    Username VARCHAR(100)
+    
+)
